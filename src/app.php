c2412d82db6c49bace55a1e577a751a8a41d7de3
Anton Smirnov <sandfox+git@sandfox.me>
2025-07-31 15:41:32 +0300

    Control by environment

--- src/app.php
+++ src/app.php
@@ -14,6 +14,6 @@ $app->group('/service.php', function (RouteCollectorProxy $service) {
     $service->get('/currencies', [ServiceController::class, 'currencies']);
 });
 
-$app->addErrorMiddleware(false, false, false);
+$app->addErrorMiddleware(getenv('DEBUG') === '1', false, false);
 
 return $app;
